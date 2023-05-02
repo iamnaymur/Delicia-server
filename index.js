57aefc051dda42942e7021c2c84439f9f0f3df39
iamnaymur <nrtusher1@gmail.com>
2023-05-02 14:29:33 +0600

    gitignore added

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+const chiefs = require("./data/chiefs.json");
+
+app.get("/", (req, res) => {
+  res.send("Delicia server is running");
+});
+
+app.get("/chiefs", (req, res) => {
+  res.send(chiefs);
+});
+
+app.get("/chiefs/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const recipe = chiefs.find((n) => n.id === id);
+  res.send(recipe);
+});
+
+app.listen(port, () => {
+  console.log(`Delcia api is running on port ${port}`);
+});
